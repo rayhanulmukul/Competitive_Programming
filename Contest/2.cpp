5554160f5afcb26618546803f4f6ac3ea6172567
Md Rayhanul Islam <73143090+rayhanulmukul@users.noreply.github.com>
2025-06-14 18:20:28 +0600

    Leetcode Problme

--- Contest/2.cpp
+++ Contest/2.cpp
@@ -1,47 +1,25 @@
-/*بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيمِ*/
-#include <bits/stdc++.h>
-using namespace std;
+class Solution {
+public:
+    int minMaxDifference(int num) {
+        string s = to_string(num);
 
-#define ll long long int
-#define int ll
-#define ld long double
-#define pb push_back
-#define ft front()
-#define bk back()
-#define pi 2*acos(0.0)
-#define gap ' '
-#define en '\n'
-#define endl en
-#define mem(a, b) memset(a, b, sizeof(a))
-#ifdef TESLA
-#include "dbg.h"
-#else
-#define dbg(...)
-#endif
-mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
-#define rng(x,y) uniform_int_distribution<int>(x,y)(rng)
-#define F0R(i,a,b) for (int i = (a); i < (b); ++i)
-#define FOR(i,a) F0R(i,0,a)
-const int MOD = 1e9+7;
-const int MAX = 2e5+5;
-const int INF = 1e18;
-int dx[] = {0, 0, +1, -1, -1, +1, -1, +1};
-int dy[] = {+1, -1, 0, 0, +1, +1, -1, -1};
+        string mx = "", mn = "";
+        int n = s.size();
 
-void solve(int tt){
-    int n;
-    cin >> n;
-    cout << n << en;
-}
-int32_t main(){
-#ifndef DEBUG
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-#endif
-    int t = 1;
-    cin >> t;
-    for(int i = 1; i <= t; i++){
-        solve(i);
+        for(int i = 0; i < n; i++){
+            if(s[i] != '9'){
+                str = s[i];
+                break;
+            }
+        }
+
+        for(int i = 0; i < n; i++){
+            if(str == s[i]) mx += '9';
+            else mx += s[i];
+        }
+
+        cout << mx << endl;
+
+        return 0;
     }
-    return 0;
-}
+};
